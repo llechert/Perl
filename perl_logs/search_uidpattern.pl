35d8422eef8edc15150acd59e461feffeb5d0c59
llechert <lukasz.lechert@gmail.com>
2018-08-25 16:32:19 +0200

    @ARGV added

--- perl_logs/search_uidpattern.pl
+++ perl_logs/search_uidpattern.pl
@@ -3,10 +3,22 @@ use strict;
 my $line = '';
 my $pattern = '53c4a1c4-3019-83d4-02c2611dde4f';
 
+if (scalar(@ARGV) > 0)
+{
+  $pattern = $ARGV[0];
+}
+
+my $counter = 0;
 open(IN,'logs.txt') or die("Could not open log file.");
 foreach (<IN>) {
     $line = $_;
     chomp;
-    print $line if (/$pattern/);
+    if (/$pattern/)
+    {
+      print $line;
+      $counter++;
+    }
 }
 close(IN);
+print "Number of elements: ".$counter;
+
