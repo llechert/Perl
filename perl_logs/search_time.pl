fac9dac3d03397a25d5de2d27a6979b97e2b4d74
llechert <lukasz.lechert@gmail.com>
2018-08-25 15:17:13 +0200

    $time regexp added

--- perl_logs/search_time.pl
+++ perl_logs/search_time.pl
@@ -1,14 +1,14 @@
 use strict;
 
 my $line = '';
-my $timeFrom = '09:03:27.890';
-my $timeTo = '09:04:30.890';
+my $timeFrom = '08:03:25.890';
+my $timeTo = '08:03:31.890';
 
 open(IN,'logs.txt') or die("Could not open log file.");
 foreach my $orgLine (<IN>) {
     $line = $orgLine;
     chomp($line); 
     (my $date, my $time, my $uid, my $char, my $user, my $text) = split('\t', $line);
-    print $orgLine if (defined $time and $time ge $timeFrom and $time le $timeTo);
+    print $orgLine if (defined $time and $time =~ /(\d+):(\d+):(\d+).(\d+)/ and $time ge $timeFrom and $time le $timeTo);
 }
 close(IN);
