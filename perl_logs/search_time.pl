0a6bc4d699b94b670910f49da39be91224a4b813
llechert <lukasz.lechert@gmail.com>
2018-08-25 15:22:43 +0200

    Counter  added

--- perl_logs/search_time.pl
+++ perl_logs/search_time.pl
@@ -4,11 +4,17 @@ my $line = '';
 my $timeFrom = '08:03:25.890';
 my $timeTo = '08:03:31.890';
 
+my $counter = 0;
 open(IN,'logs.txt') or die("Could not open log file.");
 foreach my $orgLine (<IN>) {
     $line = $orgLine;
     chomp($line); 
     (my $date, my $time, my $uid, my $char, my $user, my $text) = split('\t', $line);
-    print $orgLine if (defined $time and $time =~ /(\d+):(\d+):(\d+).(\d+)/ and $time ge $timeFrom and $time le $timeTo);
+    if (defined $time and $time =~ /(\d+):(\d+):(\d+).(\d+)/ and $time ge $timeFrom and $time le $timeTo)
+    {
+      print $orgLine;
+      $counter++;	
+    }          
 }
+print "Number of elements: ".$counter;
 close(IN);
